ac824a389cb6c1fd2856a0bab9441f7e1394bc5d
Lucas Rasmusson <lucasrasmusson@Lucas-MacBook-Pro.local>
2022-11-02 16:48:29 -0600

    newest version of main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 import random
 from sqlite3 import Error
@@ -6,15 +5,15 @@ import getpass
 import os
 import search
 
+
 # Code from group member Jonathen Adsit
 def clearScreen():
     # Clear the output
-    if(os.name == "nt"):
+    if (os.name == "nt"):
         os.system('cls')
     else:
         os.system('clear')
     return
-
 
 
 # Creates a database connection
@@ -51,13 +50,11 @@ def login(databaseFile):
             connection.close()
             x = input("Would you like to try again? y/n: ")
             if x == "y":
-                login(databaseFile)
+                result = login(databaseFile)
             else:
                 print("Goodbye!")
 
         # add to list if login type is user
-        print("right before appending:", result)
-        print("result: ", result)
         result.append("user")
 
         return result
@@ -68,9 +65,10 @@ def login(databaseFile):
 
         connection = sqlite3.connect(databaseFile)
         cur = connection.cursor()
-        query = f"SELECT a.aid, a.name FROM artists a" \
-                f" WHERE a.aid = '{aidEntered}' AND a.pwd = '{passwordEntered}';"
-        cur.execute(query)
+        # query = f"SELECT a.aid, a.name FROM artists a" \
+        #     f" WHERE a.aid = '{aidEntered}' AND a.pwd = '{passwordEntered}';"
+        cur.execute(f"SELECT a.aid, a.name FROM artists a" \
+                    f" WHERE a.aid = '{aidEntered}' AND a.pwd = '{passwordEntered}';")
 
         # x is a temporary variable
         x = cur.fetchall()
@@ -107,104 +105,89 @@ def login(databaseFile):
 
 def startSession(id):
     connection = sqlite3.connect(databaseFile)
-    cur = connection.cursor()
-    # What to do about end time in start session function?
-    # How to get unique session number?
-    sno = random.randint(0,100000)
-    print("sno: ", sno)
-    print("type sno: ", type(sno))
-    print("type id: ", type(id))
+    sno = random.randint(0, 100000)
 
-    # What to do about end date initially?
-    connection.execute(f"INSERT into sessions(uid, sno, start, end) VALUES (?,?,?,?)", (id, sno, sqlite3.Date('now'), (None,)))
+    # Start a session by inserting values into  sessions table, initialize the end date as a NULL value
+    connection.execute(f"INSERT into sessions(uid, sno, start, end) VALUES (?,?, date('now'), Null)", (id, sno))
     connection.commit()
     connection.close()
 
-    return
+    return sno
 
 
-def endSession(id):
+def endSession(id, sno):
     connection = sqlite3.connect(databaseFile)
     cur = connection.cursor()
 
-    query = f"SELECT s.sno, s.start FROM sessions s, users u" \
-            f" WHERE s.uid = u.uid;"
-    cur.execute(query)
+    cur.execute(f"SELECT s.sno, s.start, s.uid FROM sessions s, users u" \
+                f" WHERE s.uid = u.uid;")
+    # y is a temporary variable to store the results of the query
     y = cur.fetchall()
-    print("yyyyyyy: ", y)
 
-
-    #updateSessions(connection, (id,y[0][1], y[0][2], y[0][3]))
-
-    #?????
-    connection.execute(f"INSERT into sessions(uid, sno, start, end) VALUES (?,?,?,?)", (id, y[0][1], y[0][2], sqlite3.Date('now')))
-    connection.commit()
+    # update the sessions table to include coresponding end date
+    updateSessions(connection, (id, sno, y[0][1]))
 
     connection.close()
 
-'''
-def updateSessions(connection, data):
-    # function to update a table
 
-    query = """ UPDATE sessions
-                set uid = ?,
-                    sno = ?,
-                    start = ?,
-                    end = ?
-                WHERE sno = ?
-    """
-    cur = connection.cursor()
-    cur.execute(query, data)
+def updateSessions(connection, data):
+    # function to update a the sessions table
+
+    connection.execute(f" UPDATE sessions "
+                       f"set uid = ?,"
+                       f"sno = ?,"
+                       f"start = ?,"
+                       f"end = date('now')"
+                       f"WHERE sno = ?"
+                       , (data[0], data[1], data[2], data[1]))
+
     connection.commit()
-'''
+
 
 if __name__ == '__main__':
-    databaseFile = "/Users/lucasrasmusson/Documents/CMPUT291/miniProject1/miniProject1.db"
-    createDatabaseConnection(databaseFile)
-
-    data = login(databaseFile)
-
-    clearScreen()
-
-    print("we made it here")
-
-    print("data: ", data)
-
-    print("name: ", data[0][1])
-
-    #Start session here
-    print("Time to start the session #1")
-    print("data[0][0]: ", data[0][0])
-    startSession(data[0][0])
-
-    if data[1] == "user":
-        loginType = "user"
-    else:
-        loginType = "artist"
-
-    logout = False
+    # Main loop for running program
     while 1:
-        randomInput = input("Whatever input:")
-        if randomInput == "logout":
-            endSession(data[0][0])
-            # end session here
-            logout = True
+        databaseFile = "/Users/lucasrasmusson/Documents/CMPUT291/miniProject1/miniProject1.db"
+        createDatabaseConnection(databaseFile)
+
+        # data stores id,
+        data = login(databaseFile)
+
+        clearScreen()
+
+        # Start session here
+        sno = startSession(data[0][0])
+
+        # Establish login type
+        if data[1] == "user":
+            loginType = "user"
+        else:
+            loginType = "artist"
+
+        # set logout initially
+        logout = False
+        while 1:
+            randomInput = input("Whatever input:")
+            if randomInput == "logout":
+                endSession(data[0][0], sno)
+                # end session here
+                logout = True
+            if randomInput == "exit":
+                # end session here as well
+                # is exit is entered break out of the inner loop
+                endSession(data[0][0], sno)
+                break
+            # search for songs using function from search.py
+            if randomInput == "search songs":
+                search.searchSongs()
+            # search for artists using function from search.py
+            if randomInput == "search artists":
+                search.searchArtists()
+
+            # restart loop at login if logout is true
+            if logout:
+                break
+        # is exit is entered break out of the outer loop
         if randomInput == "exit":
-            # end session here as well
+
             break
-        if randomInput == "search songs":
-            search.searchSongs()
-        if randomInput == "search artists":
-            search.searchArtists()
-
-        if logout:
-            data = login(databaseFile)
-            if data[1] == "user":
-                loginType = "user"
-            else:
-                loginType = "artist"
-
-            # Start session here
-            print("Time to start the session 2")
-
-
